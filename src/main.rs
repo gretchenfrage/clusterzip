3950a835105c2ce215310f68c64029a79ec58278
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2018-03-26 17:04:24 -0500

    get host reading working

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,55 @@ pub extern crate num_cpus;
 pub extern crate rand;
 pub extern crate time;
 pub extern crate stopwatch;
+pub extern crate serde;
+pub extern crate tokio;
+pub extern crate local_ip;
+pub extern crate ssh2 as ssh;
+pub extern crate file;
 
 pub mod threading;
 
+use std::process::exit;
+use std::io;
+use std::io::Write;
+use std::path::Path;
+
+fn prompt_line(prompt: &str) -> String {
+    print!("{}:\n> ", prompt);
+    io::stdout().flush().unwrap();
+    let mut result = String::new();
+    io::stdin().read_line(&mut result).expect("failed to read line");
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    match args.get(1) {
+        Some(s) if s == "maestro" => {
+            println!("running master");
+
+            let address_path = prompt_line("provide helper address file");
+            let addresses_file = Path::new(address_path.trim());
+            let addresses = match file::get_text(&addresses_file) {
+                Ok(text) => text,
+                Err(err) => {
+                    eprintln!("failed to read text from file {:?}:", addresses_file);
+                    eprintln!("{:?}", err);
+                    exit(1);
+                }
+            };
+
+            for address in addresses.trim().split("\n") {
+                println!("address: {}", address);
+            }
+        },
+        Some(s) if s == "helper" => {
+            println!("running helper");
+        },
+        other => {
+            eprintln!("usage: ./clusterzip <maestro/helper>");
+            eprintln!("invalid arg: {:?}", other);
+            exit(1);
+        }
+    };
 }
